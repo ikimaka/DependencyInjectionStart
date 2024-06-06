8411a1a23ad68810756e452cd73d46919573f890
ikimaka <ikimaka@yandex.ru>
2024-06-06 13:16:07 +0300

    Use Factory instead of Builder. Send parameters in Factory — best way

--- app/src/main/java/com/ikimaka/dependencyinjectionstart/example2/di/ApplicationComponent.kt
+++ app/src/main/java/com/ikimaka/dependencyinjectionstart/example2/di/ApplicationComponent.kt
@@ -10,14 +10,12 @@ interface ApplicationComponent {
 
     fun inject(activity: MainActivity)
 
-    @Component.Builder
-    interface ApplicationComponentBuilder {
-        @BindsInstance
-        fun context(context: Context): ApplicationComponentBuilder
-        @BindsInstance
-        fun timeMillis(timeMillis: Long): ApplicationComponentBuilder
-
-        fun build(): ApplicationComponent
+    @Component.Factory
+    interface ApplicationComponentFactory {
+        fun create(
+            @BindsInstance context: Context,
+            @BindsInstance timeMillis: Long
+        ): ApplicationComponent
     }
 
 }
