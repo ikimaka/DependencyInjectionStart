04c4784482b66778ac88f3708a3f661cd7262ab5
ikimaka <ikimaka@yandex.ru>
2024-06-07 06:20:42 +0300

    Create View Model: way 1

--- app/src/main/java/com/ikimaka/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/ikimaka/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ikimaka.dependencyinjectionstart.example2.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.ikimaka.dependencyinjectionstart.R
 import com.ikimaka.dependencyinjectionstart.example2.ExampleApp
 import com.ikimaka.dependencyinjectionstart.example2.data.database.ExampleDatabase
@@ -12,7 +13,15 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
 
     @Inject
-    lateinit var viewModel: ExampleViewModel
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val viewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[ExampleViewModel::class.java]
+    }
+
+    private val viewModel2 by lazy {
+        ViewModelProvider(this, viewModelFactory)[ExampleViewModel2::class.java]
+    }
 
     private val component by lazy {
         (application as ExampleApp).component
@@ -24,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         viewModel.method()
+        viewModel2.method()
 
 //        val activity = Activity()
 //        activity.computer.toString()
